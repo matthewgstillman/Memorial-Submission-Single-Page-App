971d7882f0c001cde212c2796836b96c10958129
matthewgstillman <matthewgstillman@gmail.com>
2024-08-17 10:36:16 -0600

    stylistic and functional updates

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FormComponent from './components/FormComponent';
+import './styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <>
+      <FormComponent />
+    </>
+  );
+};
+
+export default App;
--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import memorialPic from '../images/memorial-pic.jpg';
 
 const FormComponent = () => {
   const [formData, setFormData] = useState({
@@ -21,29 +23,79 @@ const FormComponent = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>First Name:</label>
-      <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} required />
+    <div className="mainContainer">
+        <Row className="justify-content-center">
+          <Col md={8}>
+            <img src={memorialPic} alt="Memorial Pic" className="img-fluid mb-4" />
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="formFirstName">
+                <Form.Label>First Name:</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  required
+                />
+              </Form.Group>
 
-      <label>Last Name:</label>
-      <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} required />
+              <Form.Group controlId="formLastName">
+                <Form.Label>Last Name:</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  required
+                />
+              </Form.Group>
 
-      <label>Email:</label>
-      <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+              <Form.Group controlId="formEmail">
+                <Form.Label>Email:</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+              </Form.Group>
 
-      <label>How You Know Susan:</label>
-      <select name="relation" value={formData.relation} onChange={handleChange} required>
-        <option value="">Select an option</option>
-        <option value="friend">Friend</option>
-        <option value="family">Family</option>
-        <option value="colleague">Colleague</option>
-      </select>
+              <Form.Group controlId="formRelation">
+                <Form.Label>How You Know Susan:</Form.Label>
+                <Form.Control
+                  as="select"
+                  name="relation"
+                  value={formData.relation}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="">Select an option</option>
+                  <option value="friend">Friend</option>
+                  <option value="family">Family</option>
+                  <option value="colleague">Colleague</option>
+                </Form.Control>
+              </Form.Group>
 
-      <label>Message:</label>
-      <textarea name="message" value={formData.message} onChange={handleChange} required />
+              <Form.Group controlId="formMessage">
+                <Form.Label>Message:</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  name="message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  rows={3}
+                  required
+                />
+              </Form.Group>
 
-      <button type="submit">Submit</button>
-    </form>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+    </div>
   );
 };
 
